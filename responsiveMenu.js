28356cc0134a44aec9868851e1adcd4b12be4eaf
confett0 <118047961+confett0@users.noreply.github.com>
2025-12-09 13:44:54 +0100

    Minor fixes

--- responsiveMenu.js
+++ responsiveMenu.js
@@ -3,6 +3,7 @@ const menu = document.querySelector(".menu");
 
 const toggleMenu = () => {
    menu.classList.toggle("visible");
+   console.log("menu toggled")
 };
 
 toggle.addEventListener("click", toggleMenu);
